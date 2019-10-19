ccac88084ee6174e14472f4f93e07face32f6535
south <south@gmail.com>
2019-10-19 22:15:41 +0800

    aa

--- include/CalcViewController.h
+++ include/CalcViewController.h
@@ -3,11 +3,16 @@
 #import <Cocoa/Cocoa.h>
 
 @interface CalcViewController : NSViewController{
+    NSButton *calcBtn;
+    NSButton *cleanBtn;
+    NSTextField *textFiled;
 }
 
 +(void)showWindow;
 -(void)loadView;
 -(void)createUI;
 -(void)onBtnClick:(NSButton *)btn;
+-(void)onCalcBtnClick:(NSButton *)btn;
+-(void)onCleanBtnClick:(NSButton *)btn;
 
 @end
--- src/CalcViewController.m
+++ src/CalcViewController.m
@@ -3,6 +3,62 @@
 
 @implementation CalcViewController
 
+-(void) loadView {
+    NSLog(@"loadView start");
+    [super loadView];
+    self.view = [[NSView alloc] initWithFrame:NSMakeRect(0,0,1000, 500)]; 
+    [self createUI];
+}
+
+-(void)createUI{
+    calcBtn = [[NSButton alloc] init];
+    calcBtn.frame = NSMakeRect(0, 0, 64, 64);
+    [calcBtn setTarget:self];
+    [calcBtn setAction:@selector(onCalcBtnClick:)];
+    [calcBtn setTitle:@"计算"];
+    [self.view addSubview:calcBtn];
+
+    cleanBtn = [[NSButton alloc] init];
+    cleanBtn.frame = NSMakeRect(128, 0, 64, 64);
+    [cleanBtn setTarget:self];
+    [cleanBtn setAction:@selector(onCleanBtnClick:)];
+    [cleanBtn setTitle:@"清空"];
+    [self.view addSubview:cleanBtn];
+
+    for(int i = 0; i < 10 ; i ++ ){
+        NSButton *btn = [[NSButton alloc] init];
+        NSString *title = [[NSString alloc] initWithFormat:@"%d",i];
+        [btn setTitle:title];
+        btn.tag = i;
+        btn.frame= NSMakeRect(i * 70, 120, 64, 64);
+        [self.view addSubview:btn];
+
+        [btn setTarget:self];
+        [btn setAction:@selector(onBtnClick:)];
+    }
+
+    textFiled = [[NSTextField alloc] init];
+    textFiled.frame = NSMakeRect(0,300,300,64);
+    [self.view addSubview:textFiled];
+}
+
+-(void)onCalcBtnClick:(NSButton *)btn
+{
+    NSLog(@"onCalcBtnClick");
+}
+
+-(void)onCleanBtnClick:(NSButton *)btn
+{
+    NSLog(@"onCleanBtnClick");
+}
+
+
+-(void)onBtnClick:(NSButton *)btn
+{
+    NSLog(@"onBtnClickmake %d",btn.tag);
+}
+
+
 +(void)showWindow{
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSApplication *app = [NSApplication sharedApplication];
@@ -28,28 +84,6 @@
 
     [NSApp run];
     [pool retain];
-}
-
--(void) loadView {
-    NSLog(@"loadView start");
-    [super loadView];
-    self.view = [[NSView alloc] initWithFrame:NSMakeRect(0,0,1000, 500)]; 
-    [self createUI];
-}
--(void)createUI{
-    NSButton *btn = [[NSButton alloc] init];
-    [btn setTitle:@"确认"];
-    btn.tag = 1;
-    btn.frame= NSMakeRect(0, 0, 64, 64);
-    [self.view addSubview:btn];
-
-    [btn setTarget:self];
-    [btn setAction:@selector(onBtnClick:)];
-}
-
--(void)onBtnClick:(NSButton *)btn
-{
-    NSLog(@"onBtnClickmake %d",btn.tag);
 }
 
 @end
